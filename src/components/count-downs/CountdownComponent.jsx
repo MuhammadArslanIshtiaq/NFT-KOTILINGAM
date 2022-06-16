83de20ee8129af40472570d1b939f711ca7663d7
Muhammad Arslan <muhammadarslanishtiaq@gmail.com>
2022-06-16 12:17:04 +0500

    counter updated

--- src/components/count-downs/CountdownComponent.jsx
+++ src/components/count-downs/CountdownComponent.jsx
@@ -4,9 +4,37 @@ import Countdown from "react-countdown";
 const CountdownComponent = () => {
   // Random component
   const Completionist = () => (
-    <span className="text-[1.2rem] font-normal text-center block text-primary">
-      You are good to go!
-    </span>
+    <div className="mt-6">
+      <div className="flex justify-evenly gap-3">
+        <span>
+          <h1 className="text-[1.5rem] font-normal text-center">0</h1>
+          <small className="block text-[0.9rem] font-normal text-primary capitalize">
+            Days
+          </small>
+        </span>
+        <span className="text-[1.4rem] font-normal">:</span>
+        <span>
+          <h1 className="text-[1.5rem] font-normal text-center">0</h1>
+          <small className="block text-[0.9rem] font-normal text-primary capitalize">
+            Hours
+          </small>
+        </span>
+        <span className="text-[1.4rem] font-normal">:</span>
+        <span>
+          <h1 className="text-[1.5rem] font-normal text-center">0</h1>
+          <small className="block text-[0.9rem] font-normal text-primary capitalize">
+            minutes
+          </small>
+        </span>
+        <span className="text-[1.4rem] font-normal">:</span>
+        <span>
+          <h1 className="text-[1.5rem] font-normal text-center">0</h1>
+          <small className="block text-[0.9rem] font-normal text-primary capitalize">
+            seconds
+          </small>
+        </span>
+      </div>
+    </div>
   );
 
   // Renderer callback with condition
@@ -50,7 +78,7 @@ const CountdownComponent = () => {
 
   return (
     // <Countdown date={"June 17, 2022 20:00:00 GMT-00:00"} renderer={renderer} />
-    <Countdown date={""} renderer={renderer} />
+    <Countdown date={"June 15, 2022 20:00:00 GMT-00:00"} renderer={renderer} />
   );
 };
 
